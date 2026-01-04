423356cc45495ac6108a59066a9f135bc5f95f8c
Andy Honeycutt <ahoneycutt@gmail.com>
2026-01-04 12:17:28 -0700

    use brick manager

--- player/label.gd
+++ player/label.gd
@@ -1,11 +1,12 @@
 extends Label
 
+func _on_currency_manager_currency_changed(amount: int):
+	# Update the label text when the currency changes
+	self.text = "Bricks: " + str(amount)
+
+
 func _ready():
 	# Connect the signal via code if not done in the editor
 	Bricksmanager.bricks_changed.connect(_on_currency_manager_currency_changed)
 	# Initialize the label text with the current amount
 	_on_currency_manager_currency_changed(Bricksmanager.total_bricks)
-
-func _on_currency_manager_currency_changed(amount: int):
-	# Update the label text when the currency changes
-	text = "Bricks: " + str(amount)
--- player/temporary_level.gd
+++ player/temporary_level.gd
@@ -1,5 +1,1 @@
 extends Node3D
-
-
-func _on_area_3d_entered_area_3d(CharacterBody3D: Variant) -> void:
-	%CharacterBody3D
