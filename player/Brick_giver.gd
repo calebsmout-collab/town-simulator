4bfa2a4a2a95fe639d75ee4c1856a0f4f653a736
calebsmout-collab <calebsmout@gmail.com>
2026-01-06 17:36:48 -0700

    Brick_giver is fixed and works now I need to make it so theres a limit to how many bricks you can have at a time and make it so that the buildings dont take bricks when there already built.
    It works and everything so on to the stuff I said in the summary.

--- player/Brick_giver.gd
+++ player/Brick_giver.gd
@@ -2,8 +2,7 @@ extends Area3D
 
 signal entered_area_3D(CharacterBody3D)
 
-
-var bricks_added = 10
+var cost_of_building = 10
 
 
 
@@ -11,9 +10,9 @@ func _ready():
 	self.body_entered.connect(_on_Area3D_body_entered)
 	
 func _on_Area3D_body_entered(body: Node3D):
-	if body is not CharacterBody3D:
+	if body is CharacterBody3D:
 		
 		
-		Bricksmanager.add_bricks(bricks_added)
+		Bricksmanager.add_coins(cost_of_building)
 	
 	entered_area_3D.emit(CharacterBody3D)
