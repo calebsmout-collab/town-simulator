4799ae1f7c42095e1e813319ea35691ebbd61481
calebsmout-collab <calebsmout@gmail.com>
2026-01-09 14:13:31 -0700

    Area 3D update
    Trying to make it so that bricks arent taken when its already built.

--- player/area_3d.gd
+++ player/area_3d.gd
@@ -7,19 +7,21 @@ var cost_of_building = 10
 var number_of_bricks_needed = 10
 
 
-func _ready():
-	self.body_entered.connect(_on_Area3D_body_entered)
-	
 func _on_Area3D_body_entered(body: Node3D):
 	if body is not CharacterBody3D:
 		return
 	if not Bricksmanager.total_bricks:
 		return
+
+	var collider = self.get_parent().get_node("StaticBody3D/CollisionShape3D")
+	if not collider.disabled:
+		return
+	
 	if Bricksmanager.total_bricks >= number_of_bricks_needed:
 		# remove transparency
 		self.get_parent().set_surface_override_material(0, null)
 		# enable collider
-		self.get_parent().get_node("StaticBody3D/CollisionShape3D").call_deferred("set_disabled", false)
+		collider.call_deferred("set_disabled", false)
 		# subtract bricks
 		Bricksmanager.subtract_coins(cost_of_building)
 	
