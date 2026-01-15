18ba7a46b0def3a8ef2c37f4122ae99cd4ffe47e
Andy Honeycutt <ahoneycutt@gmail.com>
2026-01-14 21:04:19 -0700

    add _ready function, get collider from parent node
    The `_ready` method was removed from the area3d but is necessary to connect the area3d to the `_on_Area3D_body_entered` function.
    
    Also, assigning the collisionshape3d node to a variable was causing problems with accessing the actual node.

--- player/area_3d.gd
+++ player/area_3d.gd
@@ -6,23 +6,29 @@ signal entered_area_3D(CharacterBody3D)
 var cost_of_building = 10
 var number_of_bricks_needed = 10
 
+func _ready():
+	self.body_entered.connect(_on_Area3D_body_entered)
+
 
 func _on_Area3D_body_entered(body: Node3D):
 	if body is not CharacterBody3D:
 		print("body is not player")
 		return
+		
 	if not Bricksmanager.total_bricks:
+		print("player doesn't have bricks")
 		return
 
-	var collider = self.get_parent().get_node("StaticBody3D/CollisionShape3D")
-	if not collider.disabled:
+	if not self.get_parent().get_node("StaticBody3D/CollisionShape3D"):
+		print("collider is enabled")
 		return
 	
 	if Bricksmanager.total_bricks >= number_of_bricks_needed:
+		print("subtracting bricks...")
 		# remove transparency
 		self.get_parent().set_surface_override_material(0, null)
 		# enable collider
-		collider.call_deferred("set_disabled", false)
+		self.get_parent().get_node("StaticBody3D/CollisionShape3D").call_deferred("set_disabled", false)
 		# subtract bricks
 		Bricksmanager.subtract_coins(cost_of_building)
 	
